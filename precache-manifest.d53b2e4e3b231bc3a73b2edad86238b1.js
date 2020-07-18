7062dcf531976757d0cbb71b05d350d4f7442087
keralex <kerlis.aguado26@gmail.com>
2020-07-18 14:17:08 -0300

    Updates

---
+++ precache-manifest.d53b2e4e3b231bc3a73b2edad86238b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad31dc039e41c49bb02357a6bd6f5ff9",
+    "url": "/ToDo-React/index.html"
+  },
+  {
+    "revision": "75fea9c4d560f83d768b",
+    "url": "/ToDo-React/static/css/main.f94390fb.chunk.css"
+  },
+  {
+    "revision": "6a3e563be3203583492c",
+    "url": "/ToDo-React/static/js/2.561c52a4.chunk.js"
+  },
+  {
+    "revision": "75fea9c4d560f83d768b",
+    "url": "/ToDo-React/static/js/main.ae45bc39.chunk.js"
+  },
+  {
+    "revision": "836b1f0d00084a7c8c57",
+    "url": "/ToDo-React/static/js/runtime~main.fd83de53.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ToDo-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
